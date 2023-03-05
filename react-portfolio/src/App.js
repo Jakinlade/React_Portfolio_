46a252d6f1d771b1167855d0d2b5cf009cb9ae93
Jakinlade <jj.47.johnson@gmail.com>
2023-03-05 23:01:14 +0000

    fixed paths in index.js

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,9 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header from "./Components/Header";
+import Header from "./Components/Header.jsx";
 import Home from "./Components/Home";
 import ProjectGallery from "./Components/ProjectGallery";
-import Project from "./Components/Project";
+import Project from "./Components/Project.jsx";
 import Contact from "./Components/Contact";
+import About from "./Components/About";
 
 function App() {
   return (
@@ -12,9 +13,11 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" component={Home} />
+          <Route path="/about" component={About} />
           <Route path="/projects" component={ProjectGallery} />
           <Route path="/projects/:id" component={Project} />
           <Route path="/contact" component={Contact} />
+          <Route path="/about" component={About} />
         </Routes>
       </div>
     </Router>
---
+++ react-portfolio/src/Components/Header.jsx
@@ -0,0 +1,34 @@
+import { NavLink } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/" activeClassName="active">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/projects" activeClassName="active">
+              Projects
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" activeClassName="active">
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact" activeClassName="active">
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;

5a002c7925af97ee6df3a134e7071c99b74bf18b
Jakinlade <jj.47.johnson@gmail.com>
2023-03-05 22:06:50 +0000

    created folder structure and app.js start

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './Header';
+import Home from './Home';
+import ProjectGallery from './ProjectGallery';
+import Project from './Project';
+import Contact from './Contact';
+
+function App() {
+ 
+}
+
+export default App;

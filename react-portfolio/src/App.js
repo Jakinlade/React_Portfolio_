7d4d365becdd61eaa050c7f21669a9315a87f609
Jakinlade <jj.47.johnson@gmail.com>
2023-03-05 22:17:46 +0000

    resolved path errors and installed react router dom

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,12 +1,24 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Header from './Header';
-import Home from './Home';
-import ProjectGallery from './ProjectGallery';
-import Project from './Project';
-import Contact from './Contact';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import ProjectGallery from "./Components/ProjectGallery";
+import Project from "./Components/Project";
+import Contact from "./Components/Contact";
 
 function App() {
- 
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" component={Home} />
+          <Route path="/projects" component={ProjectGallery} />
+          <Route path="/projects/:id" component={Project} />
+          <Route path="/contact" component={Contact} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
